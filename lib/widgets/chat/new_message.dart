1d7ac6c6b3ccbee66dc18ea44af1d8d446f26ade
David Mulyawan <91412794+davdmoo@users.noreply.github.com>
2022-03-04 13:52:48 +0700

    Feat: add username when creating message

--- lib/widgets/chat/new_message.dart
+++ lib/widgets/chat/new_message.dart
@@ -15,12 +15,14 @@ class _NewMessageState extends State<NewMessage> {
   Future<void> _sendMessage() async {
     FocusScope.of(context).unfocus();
     final user = await FirebaseAuth.instance.currentUser();
+    final userData = await Firestore.instance.collection("users").document(user.uid).get();
 
     Firestore.instance.collection("chat")
       .add({
         "text": _enteredMessage,
         "createdAt": Timestamp.now(), // from cloud firestore
         "userId": user.uid,
+        "username": userData["username"],
       });
     
     _controller.clear();
